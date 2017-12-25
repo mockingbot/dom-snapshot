4cb20481538c142a3e5c147e7ffef23e33e70746
dr-js <dr@dr.city>
2017-12-25 16:50:04 +0800

    dom-snapshot@0.0.1

--- source/index.js
+++ source/index.js
@@ -4,18 +4,19 @@ import { createHTMLFragCollector } from './HTMLFragCollector'
 
 const { prepareHTMLString, prepareCSSString, prepareSVGString, prepareImageElement, prepareCanvasElement } = Prepare
 
-const getCanvasFromDOMFrag = async ({
-  urlMap, htmlFragList,
-  cssLinkHrefList, cssFragList,
-  width, height
-}) => {
-  let htmlString
-  let cssString
-  let domString
-  let svgString
-  let imageElement
-  let canvasElement
+const createSnapshotFromHTMLSourceList = async ({ htmlSourceList, width, height }) => {
+  const { getOutput, collect } = createHTMLFragCollector()
+  htmlSourceList.forEach(collect)
+  const { htmlFragList, urlMap } = getOutput()
+  __DEV__ && console.log('[generate]', { htmlFragList, urlMap })
 
+  const cssLinkHrefList = Array.from(document.getElementsByTagName('link'))
+    .map((element) => (element.rel === 'stylesheet' && element.href))
+    .filter((href) => href)
+  const cssFragList = Array.from(document.getElementsByTagName('style'))
+    .map((element) => element.innerHTML)
+
+  let htmlString, cssString, domString, svgString, imageElement, canvasElement
   try {
     htmlString = await prepareHTMLString(urlMap, htmlFragList)
     cssString = await prepareCSSString(cssLinkHrefList, cssFragList)
@@ -23,26 +24,18 @@ const getCanvasFromDOMFrag = async ({
     svgString = await prepareSVGString({ domString, width, height })
     imageElement = await prepareImageElement({ svgString, width, height })
     canvasElement = await prepareCanvasElement({ imageElement, width, height })
-
-    return {
-      htmlString,
-      cssString,
-      domString,
-      svgString,
-      imageElement,
-      canvasElement
-    }
+    return { htmlString, cssString, domString, svgString, imageElement, canvasElement }
   } catch (error) {
-    __DEV__ && console.warn('[getCanvasFromDOMFrag] error:', error, {
-      htmlString,
-      cssString,
-      domString,
-      svgString,
-      imageElement,
-      canvasElement
-    })
+    __DEV__ && console.warn('[createSnapshotFromHTMLSourceList] error:', error, { htmlString, cssString, domString, svgString, imageElement, canvasElement })
     return {}
   }
+}
+
+const createSnapshotFromElement = async (element) => {
+  const htmlSourceList = new window.XMLSerializer().serializeToString(element).split('\n')
+  const { offsetWidth: width, offsetHeight: height } = element
+  console.log('[createSnapshotFromElement]', { htmlSourceList, width, height })
+  return createSnapshotFromHTMLSourceList({ htmlSourceList, width, height })
 }
 
 export {
@@ -51,5 +44,6 @@ export {
   Download,
   Prepare,
   createHTMLFragCollector,
-  getCanvasFromDOMFrag
+  createSnapshotFromHTMLSourceList,
+  createSnapshotFromElement
 }
